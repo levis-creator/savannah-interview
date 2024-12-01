a24f4bc8c91ef60251acc5d22e4436cf8d54c29e
levis-creator <47003569+levis-creator@users.noreply.github.com>
2024-12-02 01:17:16 +0300

    Update apiUrl.ts

--- src/lib/apiUrl.ts
+++ src/lib/apiUrl.ts
@@ -1,4 +1,3 @@
-export const apiUrl= "https://jsonplaceholder.typicode.com/"
-export const publicApiUrl=process.env.NEXT_PUBLIC_API_URL
+export const apiUrl= process.env.NEXT_PUBLIC_EXTERNAL_API_URL
+export const publicApiUrl="https://jsonplaceholder.typicode.com/"
 export const baseUrl=process.env.NEXT_PUBLIC_BASE_URL
-
